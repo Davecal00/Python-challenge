564c02788cfde1499fe4b30a215d6fed242e7bc9
Davecal00 <david.calero92@gmail.com>
2023-03-10 19:46:46 -0700

    create folder

---
+++ PyBank/main.py
@@ -0,0 +1,16 @@
+import os 
+import csv
+import statistics
+monthCount = 0
+
+totalVolume = 0
+
+greatestIncrease = 0
+
+bestMonth = ''
+
+greatestDecrease = 0
+
+worstMonth = ''
+
+print(monthCount)
